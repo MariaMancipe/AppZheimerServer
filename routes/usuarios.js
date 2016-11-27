69ad621d251945168052b0f1f50fc549a035a584
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2016-11-27 14:57:00 -0500

    +pruebas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Appzheimer-Familiares' });
+    res.json({ message: 'hooray! welcome to our api!' });
 });
+
+
 
 module.exports = router;
