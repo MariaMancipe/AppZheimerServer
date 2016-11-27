d4ed35545061791eda767366f66277b15e46c91a
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2016-11-27 16:24:10 -0500

    pruebas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var usuarios =[];
 /* GET home page. */
 router.get('/:usuario_id', function(req, res, next) {
     res.json({ message: 'hooray! welcome to our api!'+req.params.usuario_id });
+});
+router.put('/:usuario_id', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!'+req.params.usuario_id });
+});
+
+router.get('/', function(req, res, next) {
+    res.json(usuarios);
+});
+
+router.post('/', function(req, res, next) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 
